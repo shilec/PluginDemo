8f662435db003e1421f28406ee401f70e5f438ed
GitHub Action <action@github.com>
2023-02-16 03:38:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/shilec/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shilec/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shilec.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shilec.plugindemo.MyBundle
 
 class MyApplicationService {
 
